507ac37179e8ce3b9aff8d8d03441f02ff2f651f
KucielKrzysztof <93817871+KucielKrzysztof@users.noreply.github.com>
2022-06-05 21:13:50 +0200

    last
    last

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,10 @@
+import numpy as np
 import os
 import pandas as pd
-from matplotlib import pyplot as plt
-import numpy as np
+from matplotlib import pyplot as plt 
 
-print(*[filename.split(".")[0] for filename in os.listdir("./reviews")], sep="\n")
-product_id = input("Podaj identyfokator produktu: ")
+print(*[filename.split(".")[0]for filename in os.listdir("./reviews")],sep="\n")
+product_id = input("Podaj identyfikator produktu ")
 
 opinions = pd.read_json("reviews/"+product_id+".json")
 
@@ -12,7 +12,6 @@ opinions_count = len(opinions)
 pros_count = opinions["pros"].astype(bool).sum()
 cons_count = opinions["cons"].astype(bool).sum()
 average_score = opinions["stars"].mean().round(2)
-print(opinions_count)
 
 recommendations = opinions["recommendation"].value_counts(dropna=False).sort_index().reindex([False, True, None], fill_value=0)
 recommendations.plot.pie(
@@ -21,17 +20,13 @@ recommendations.plot.pie(
     title = "Rekomendacje",
     labels = ["Nie polecam", "Polecam", "Nie mam zdania"],
     colors = ["crimson", "forestgreen", "lightskyblue"]
+
 )
 plt.show()
-plt.savefig("plots/"+product_id+"_recommendations.png")
-plt.close()
-####
-starss = opinions["stars"].value_counts(dropna=False).sort_index().reindex(np.arange(0,5.5,0.5), fill_value=0)
-starss.plot.bar(color = "red")
-plt.title("Gwiazdki")
-plt.xlabel("Liczba gwiazdek")
-plt.ylabel("Liczba opinii")
-starss.plot.pie(colors = ["cyan","green","orange"])
-plt.savefig("plots/"+product_id+"_stars.png")
+
+bins= np.arange(0.5,5,0.5)
+plt.hist(opinions["stars"],bins=bins+ 0.8,width=0.3)
+plt.title('Ocena')
+plt.ylabel('Recenzje')
+plt.xlabel('Ilość gwiazdek')
 plt.show()
-plt.close()
